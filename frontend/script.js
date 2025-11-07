5619f466075ab67406c48c39189bf0988c6d5aff
sumandashw16 <sumandash2004@gmail.com>
2025-11-08 00:04:35 +0530

    change in frontend

--- frontend/script.js
+++ frontend/script.js
@@ -18,7 +18,7 @@ form.addEventListener("submit", async (e) => {
   formData.append("include_audio", includeAudio);
 
   try {
-    const response = await fetch("https://pose2stick.onrender.com", {
+    const response = await fetch("https://pose2stick.onrender.com/api/process", {
       method: "POST",
       body: formData,
     });
